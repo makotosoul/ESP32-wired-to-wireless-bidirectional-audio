e0fa7c878431fc1d58f6d5d33124ed93b32bef7d
Makotosoul <148573685+makotosoul@users.noreply.github.com>
2026-04-29 10:33:08 +0300

    making auto find MAC script and chating between the two node

---
+++ peers.h
@@ -0,0 +1,11 @@
+#ifndef PEERS_H
+#define PEERS_H
+
+// Replace these with the actual MAC addresses found using get_mac.sh
+// Node A MAC Address
+uint8_t nodeAAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+// Node B MAC Address
+uint8_t nodeBAddress[] = {0xEE, 0xEE, 0xEE, 0xEE, 0xEE, 0xEE};
+
+#endif
